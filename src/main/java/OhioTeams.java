07ed0ec25ff38ce4283404e916357297708c3974
kristenrjackson <kristenrjackson7@gmail.com>
2020-12-20 16:25:27 -0500

    Added team name - Cleveland Browns

--- src/main/java/OhioTeams.java
+++ src/main/java/OhioTeams.java
@@ -1,3 +1,9 @@
 public class OhioTeams {
     private String name = "";
+
+    public static void main(String[] args) {
+        String name = "Cleveland Browns";
+        System.out.println(name);
+    }
+
 }
